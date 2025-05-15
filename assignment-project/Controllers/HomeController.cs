82b2bd0d2eb543ea658668dde8f5935548882be9
Christopher Bertrand <c.bertrandtjo@gmail.com>
2025-05-15 17:16:22 +0800

    Initial commit

---
+++ assignment-project/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index() {
+            return View();
+        }
+        [Route("about")]
+        public IActionResult About() {
+            return View();
+        }
+        [Route("contact")]
+        public IActionResult Contact() {
+            return View();
+        }
+    }
+}
