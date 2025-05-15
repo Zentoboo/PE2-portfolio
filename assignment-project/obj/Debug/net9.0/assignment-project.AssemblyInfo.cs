48077116987d6ef6c5dece6fa0f2f42de91ee9f1
Christopher Bertrand <c.bertrandtjo@gmail.com>
2025-05-15 17:22:41 +0800

    Added prior modifications

--- assignment-project/obj/Debug/net9.0/assignment-project.AssemblyInfo.cs
+++ assignment-project/obj/Debug/net9.0/assignment-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("assignment-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b2bd0d2eb543ea658668dde8f5935548882be9")]
 [assembly: System.Reflection.AssemblyProductAttribute("assignment-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("assignment-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
