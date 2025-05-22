0c6df0dfb03534faa2fd3212376049f1f651d4ef
Christopher Bertrand <c.bertrandtjo@gmail.com>
2025-05-23 03:06:36 +0800

    Update: separated About.js from its cshtml; changed naming schemes of folders to fit its actual context use.

--- assignment-project/obj/Debug/net9.0/assignment-project.AssemblyInfo.cs
+++ assignment-project/obj/Debug/net9.0/assignment-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("assignment-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d0cf84bec69f68002608d869167570a620edb5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4abab5218657f20176837e991915a7396478139d")]
 [assembly: System.Reflection.AssemblyProductAttribute("assignment-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("assignment-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
